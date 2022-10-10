4d3b9fe8715f0d9b59a24a280ddd06caecf0ceb7
Mutoi01 <pngando9@gmail.com>
2022-10-10 09:33:18 +0300

    complete

--- db/migrate/20221006105256_create_restaurants.rb
+++ db/migrate/20221006105256_create_restaurants.rb
@@ -1,4 +1,4 @@
-class CreateRestaurant < ActiveRecord::Migration[7.0]
+class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
       t.string :name
